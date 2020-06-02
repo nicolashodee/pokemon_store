d7c734cc19f85fef213e559b8a1b3debd7dc8a06
Nicolas HODEE <accounts@nicolashodee.com>
2020-06-02 16:05:40 +0100

    trying to fix heroku mailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,20 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
   default from: 'webmaster@nicolashodee.com'
   layout 'mailer'
-
-  
- 
-  def welcome_email(user)
-    #on récupère l'instance user pour ensuite pouvoir la passer à la view en @user
-    @user = user 
-
-    #on définit une variable @url qu'on utilisera dans la view d’e-mail
-    @url  = 'http://monsite.fr/login' 
-
-    # c'est cet appel à mail() qui permet d'envoyer l’e-mail en définissant destinataire et sujet.
-    mail(to: @user.email, subject: 'Bienvenue sur le Pokemon Store !') 
-  end
-
-
-
 end
