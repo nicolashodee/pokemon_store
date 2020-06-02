bab6ee8aa4ded93d3f98d20d1acd7e288f8e9827
JordanT <jo.tournerie@gmail.com>
2020-06-02 19:02:57 +0200

    cart controller + index modified

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -7,9 +7,16 @@ class CartsController < ApplicationController
     current_user.cart.update_attribute(:total_price, price)
   end
 
-  def update
-    @item = Item.find(params[:item_id])
-    join_item_cart = JoinTableItemCart.create(item: @item, cart: Cart.find_by(user: current_user))
+  def edit
+    @item = Item.find(params[:id])
+    join_item_cart = JoinTableItemCart.new(item: @item, cart: Cart.find_by(user: current_user))
+    if join_item_cart.save
+      flash[:success] = 'Bravo, tu ajouté un item à ton panier !'
+      redirect_to root_path
+    else
+      flash.now[:error] = "Une erreur s'est produite, tu n'as pas pu mettre cet item dans ton panier..."
+      render 'items#index'
+    end
   end
 
   def destroy
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ApplicationRecord
+  after_create :add_cart
   has_one :cart
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  private
+
+  def add_cart
+    cart = Cart.create(user: self)
+  end
+
 end
