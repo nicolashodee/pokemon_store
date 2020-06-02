3a5955d921506ba876f8b074450f668df70641a7
Nicolas HODEE <accounts@nicolashodee.com>
2020-06-02 15:33:09 +0100

    setting up action mailer for heroku and sendgrid

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,8 +60,8 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
 
-  # Mailer pour devise (avec letter opener pour l'instant)
-  config.action_mailer.delivery_method = :letter_opener
+  # Mailer
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
