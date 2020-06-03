665ca5822f761f195daa987188a010af06f2d98f
Nicolas HODEE <accounts@nicolashodee.com>
2020-06-03 21:40:29 +0100

    resolved last conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 2020_06_03_131950) do
-=======
-ActiveRecord::Schema.define(version: 2020_06_03_125739) do
->>>>>>> views_user_nico
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,16 +65,13 @@ ActiveRecord::Schema.define(version: 2020_06_03_125739) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-<<<<<<< HEAD
     t.boolean "is_admin", default: false
-=======
     t.string "first_name"
     t.string "last_name"
     t.text "description"
     t.date "dob"
     t.string "address"
     t.string "postcode"
->>>>>>> views_user_nico
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
