ba49630c71675acf0832f362872f6627ccfe9c5a
JordanT <jo.tournerie@gmail.com>
2020-06-03 21:04:18 +0200

    cart features v2

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -7,12 +7,8 @@ class CartsController < ApplicationController
     current_user.cart.update_attribute(:total_price, price)
   end
 
-  def edit
-    if params[:number_item]
-      number_item = params[:number_item].to_i
-    else
-      number_item = 1
-    end
+  def create
+    params[:number_item] ? number_item = params[:number_item].to_i : number_item = 1
     @item = Item.find(params[:id])
     number_item.times do
       join_item_cart = JoinTableItemCart.new(item: @item, cart: Cart.find_by(user: current_user))
@@ -27,9 +23,36 @@ class CartsController < ApplicationController
     redirect_to root_path
   end
 
+  def edit
+    params[:number_item] ? number_item = params[:number_item].to_i : number_item = 1
+    params[:item_id] ? @item = Item.find(params[:item_id]) : @item = Item.find(params[:id])
+    JoinTableItemCart.find_each do |join|
+      if join = JoinTableItemCart.find_by(item: @item, cart: Cart.find_by(user: current_user))
+        join.delete
+      end
+    end
+    number_item.times do
+      join_item_cart = JoinTableItemCart.new(item: @item, cart: Cart.find_by(user: current_user))
+      if join_item_cart.save
+        next
+      else
+        flash.now[:error] = "Une erreur s'est produite, tu n'as pas pu modifier ton panier..."
+        render 'items#index'
+      end
+    end
+    flash[:success] = 'Bravo, tu modifié ton panier !'
+    redirect_back(fallback_location: root_path)
+  end
+
   def destroy
     @item = Item.find(params[:item_id])
-    JoinTableItemCart.find_by(item: @item, cart: Cart.find_by(user: current_user)).delete
+    JoinTableItemCart.find_each do |join|
+      if join = JoinTableItemCart.find_by(item: @item, cart: Cart.find_by(user: current_user))
+        join.delete
+      end
+    end
+    flash[:notice] = "Vous avez retiré un ou plusieurs items de votre panier..."
+    redirect_to cart_path(current_user.cart)
   end
 
 end
