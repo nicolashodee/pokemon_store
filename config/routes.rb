dd705f8ef371f1bc2fc8c9beb25b749d96834c1a
Titpioupiou <carriere.aurore@orange.fr>
2020-06-03 17:28:20 +0200

    ajout Active Storage + avatar controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
+  get 'avatars/create'
   root 'items#index'
   resources :items
   resources :carts, only: [:show, :edit, :update]
   devise_for :users
+  resources :users
   namespace :admin do
     resources :items
   end
   get 'home/private'
-<<<<<<< HEAD
   resources :orders
-  
-=======
   resources :charges, only: [:new, :create]
->>>>>>> 8d3836412af2bb7042092c0a5362e71e8d4ec484
 end
