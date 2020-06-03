78e820308038d9312cef86e8c27906b59a71cbf9
Nicolas HODEE <accounts@nicolashodee.com>
2020-06-03 14:12:21 +0100

    added some columns to the DB

---
+++ db/migrate/20200603125739_add_more_infos_to_users.rb
@@ -0,0 +1,9 @@
+class AddMoreInfosToUsers < ActiveRecord::Migration[5.2]
+  def change
+    #on ajoute des infos de base que le user peut rentrer, dont sa date de naissance qui servira a calculer son age.
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :description, :text
+    add_column :users, :dob, :date
+  end
+end
