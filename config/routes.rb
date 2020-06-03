d42d8f55b05dd0df7c1c60463b5363eb32a217fe
Titpioupiou <carriere.aurore@orange.fr>
2020-06-03 17:49:02 +0200

    active storage fonctionne en local

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,14 @@ Rails.application.routes.draw do
   resources :items
   resources :carts, only: [:show, :edit, :update]
   devise_for :users
-  resources :users
   namespace :admin do
     resources :items
   end
   get 'home/private'
   resources :orders
   resources :charges, only: [:new, :create]
+
+  resources :users, only: [:show] do
+    resources :avatars, only: [:create]
+  end
 end
