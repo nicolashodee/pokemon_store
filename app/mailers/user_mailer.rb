eaeca6069030a33b0e8d6ed7d4d4a4887619ab42
Homarv <pierratmarvin3@gmail.com>
2020-06-04 16:12:23 +0200

    nouvelle branche email confirm car impossible de debug h10 heroku

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,11 @@ class UserMailer < ApplicationMailer
     @url = 'mon-site.fr'
     mail(to: @user.email, subject: 'Bienvenue chez nous !')
   end
+
+  def confirmation_command(user)
+    @user = user
+    @url = 'mon-site.fr'
+    mail(to: @user.email, subject: 'Ta commande est bien confirmé')
+  end
+  
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,10 @@ class Order < ApplicationRecord
   belongs_to :user
   has_many :join_table_order_item
   has_many :items, through: :join_table_order_item
+
+  # pour le mailer
+  after_create :confirmation_command
+  def confirmation_command
+    UserMailer.confirmation_command(self).deliver_now
+  end
 end
