26905a0e9e4f1120a47a6808f8fb2524bfa9ef2e
JordanT <jo.tournerie@gmail.com>
2020-06-04 18:09:36 +0200

    dashboard v1

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -2,7 +2,28 @@ class Admin::ItemsController < ApplicationController
   before_action :check_if_admin
 
   def index
+    @total_sell = 0
+    @count = 0
+    if Order.all.size >= 1
+      Order.all.each do |order|
+        order.items.all.each do |item|
+          @total_sell += order.item.price
+          @count += 1
+        end
+      end
+    end
+    if @count == 0
+      @average_price = 0
+    else
+      @average_price = @total_sell / @count
+    end
+  end
 
+  def destroy
+    @item = Item.find(params[:id])
+    @item.delete
+    flash[:notice] = "Vous avez supprimé un pokémon du site..."
+    redirect_back(fallback_location: root_path)
   end
 
   private
